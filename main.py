fe2eb5b201bec48b22c767208d7c4c9b971761e3
sivaldo vieira <sivaldovieiradealmeida@gmail.com>
2025-01-24 10:42:53 -0300

    Criando uma tela no aplicativo

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen
 
 GUI = Builder.load_file("main.kv")
+
+
+class HomePage(Screen):
+    pass
+
+
 
 class MainApp(App):
     def build(self):
