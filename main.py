409550318b2d669031fd3769972fdf1832659647
sivaldo vieira <sivaldovieiradealmeida@gmail.com>
2025-01-24 12:22:54 -0300

    Criando uma nova página (Ajustes)

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ from kivy.uix.screenmanager import Screen
 class HomePage(Screen):
     pass
 
-GUI = Builder.load_file("main.kv")
 
+class AjustesPage(Screen):
+    pass
+
+GUI = Builder.load_file("main.kv")
 class MainApp(App):
     def build(self):
         return GUI
