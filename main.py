63efcee9f3998ca7a2d91ddf7da588d10df1c674
sivaldo vieira <sivaldovieiradealmeida@gmail.com>
2025-01-24 13:09:46 -0300

    Criando função para Navegar entre Telas

--- main.py
+++ main.py
@@ -5,22 +5,21 @@ from kivy.uix.screenmanager import Screen
 class HomePage(Screen):
     pass
 
-
 class AjustesPage(Screen):
     pass
 
+# Carregar o arquivo KV
 GUI = Builder.load_file("main.kv")
+
 class MainApp(App):
     def build(self):
         return GUI
     
-    def on_button_press(self):
-        print("Botão pressionado!")
+    def mudar_tela(self, id_tela):
+        # Obter o gerenciador de telas usando o id
+        gerenciador_telas = self.root.ids['screen_manager']
+        # Alterar a tela atual para a tela com o id fornecido
+        gerenciador_telas.current = id_tela
 
 if __name__ == "__main__":
     MainApp().run()
-
-
-
-
-
