296f2b8914e4e8b1442ff2208c4e21c1eda81c7a
sivaldo vieira <sivaldovieiradealmeida@gmail.com>
2025-01-26 19:50:27 -0300

    Criação de Aplicativo para Celular com Kivy / Aula 17 de 56: Criando várias páginas de uma vez

--- telas.py
+++ telas.py
@@ -1,3 +1,4 @@
+from kivy.uix.filechooser import ScreenManager
 from kivy.uix.screenmanager import Screen
 
 class HomePage(Screen):
@@ -5,3 +6,27 @@ class HomePage(Screen):
 
 class AjustesPage(Screen):
     pass
+
+
+class AdicionarVendasPage(Screen):
+    pass
+
+class ListarVendedoresPage(Screen):
+    pass
+
+class FotoPerfilPage(Screen):
+    pass
+
+class AdicionarVendedorPage(Screen):
+    pass
+
+class TodasVendasPage(Screen):
+    pass
+
+
+
+#passo 1 -> criar classes no telas
+#passo 2 -> criar arquivo.kv na pasta kv
+#passo 3 -> adicionar todos arquivos.kv no main.kv
+#passo 4 adicionar todas as classes no ScreenManager no main.kv
+#passo 5 na homepage e no ajustes incluir id da tela navegacao de no botao
