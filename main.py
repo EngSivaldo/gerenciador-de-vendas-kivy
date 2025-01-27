aba34c19c5921576d55feb4dca7dd64c542e8708
sivaldo vieira <sivaldovieiradealmeida@gmail.com>
2025-01-27 08:08:32 -0300

    Criação de Aplicativo para Celular com Kivy / Aula 19 de 56: Pegando informações do Banco de Dados

--- main.py
+++ main.py
@@ -2,13 +2,26 @@ from kivy.app import App
 from kivy.lang import Builder
 from telas import *
 from botoes import *
+import requests   
 
 # Carregar o arquivo KV
 GUI = Builder.load_file("main.kv")
 
 class MainApp(App):
+    id_usuario = 1
+    #constroe parte visual
     def build(self):
         return GUI
+
+    #executa assim que ele inicia
+    def on_start(self):
+        requisicao = requests.get(f"https://apilactivovendashash-default-rtdb.firebaseio.com/{self.id_usuario}.json")
+        requisicao_dic = requisicao.json()
+        avatar = requisicao_dic['avatar']
+        print(avatar)
+        # print(requisicao.json())
+
+
     
     def mudar_tela(self, id_tela):
         # Obter o gerenciador de telas usando o id
@@ -18,3 +31,8 @@ class MainApp(App):
 
 if __name__ == "__main__":
     MainApp().run()
+
+
+#get - pegar info do bd
+#post -> enviar para o bd
+#patch -> atualizar
