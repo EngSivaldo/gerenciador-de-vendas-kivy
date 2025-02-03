e25ef4255d656a7b16e35c4e7c1cfcf93959cd06
sivaldo vieira <sivaldovieiradealmeida@gmail.com>
2025-02-02 21:48:56 -0300

    Criação de Aplicativo para Celular com Kivy / Aula 28 de 56: Exibindo Erros na Criação do Usuário

--- myfirebase.py
+++ myfirebase.py
@@ -1,4 +1,5 @@
 import requests
+from kivy.app import App
 
 
 class MyFirebase():
@@ -9,12 +10,23 @@ class MyFirebase():
     link = f'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key={self.API_KEY}'
     print(email, senha)
     info = {'email': email,
-            'senha': senha,
+            'password': senha,
             'returnSecureToken': True            
             
             }
     requisicao = requests.post(link, data=info)
     requisicao_dic = requisicao.json()
+
+    if requisicao.ok:
+      print('Usuário cadastrado!')
+    else:  #msg mudar na pagina de loginpage.kv ,label subtitle
+      mensagem_erro = requisicao_dic['error']['message']
+      meu_aplicativo = App.get_running_app()
+      pagina_login = meu_aplicativo.root.ids['loginpage']
+      pagina_login.ids['mensagem_login'].text = mensagem_erro
+      pagina_login.ids['mensagem_login'].color = (1,0,0,1)
+
+
     print(requisicao_dic)
     
 
